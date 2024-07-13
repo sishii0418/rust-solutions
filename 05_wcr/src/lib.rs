98d60080b01ed426d6a626d0e10c75e767af0ee3
Shunsuke Ishii <s.ishii@sms.ed.ac.uk>
2024-07-13 11:37:29 +0900

    passed chap5 tests

---
+++ 05_wcr/src/lib.rs
@@ -0,0 +1,186 @@
+use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    files: Vec<String>,
+    lines: bool,
+    words: bool,
+    bytes: bool,
+    chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("Shunsuke Ishii")
+        .about("Rust wc")
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILES")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("Show line count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("Show word count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Show byte count")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("Show character count")
+                .takes_value(false)
+                .conflicts_with("bytes"),
+        )
+        .get_matches();
+
+    let mut lines = matches.is_present("lines");
+    let mut words = matches.is_present("words");
+    let mut bytes = matches.is_present("bytes");
+    let chars = matches.is_present("chars");
+
+    // If all flags are false, set lines, words, and bytes to true
+    if [lines, words, bytes, chars].iter().all(|v| !v) {
+        lines = true;
+        words = true;
+        bytes = true;
+    }
+
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        lines,
+        words,
+        bytes,
+        chars,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let multiple = config.files.len() > 1;
+    let mut total_lines = 0;
+    let mut total_words = 0;
+    let mut total_bytes = 0;
+    let mut total_chars = 0;
+
+    for filename in &config.files {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file)    => {
+                let info = count(file);
+                let filename_to_show = if filename == "-" { "".to_string() } else { (" ".to_owned() + filename).to_string() };
+                if config.chars {
+                    let num_chars = info.as_ref().unwrap().num_chars;
+                    println!("{:>8}{}",
+                        num_chars.to_string(),
+                        filename_to_show);
+                } else {
+                    let num_lines = info.as_ref().unwrap().num_lines;
+                    let num_words = info.as_ref().unwrap().num_words;
+                    let num_bytes = info.as_ref().unwrap().num_bytes;
+                    println!("{}{}{}{}",
+                        if config.lines { format!("{:>8}", num_lines.to_string()) } else { "".to_string() },
+                        if config.words { format!("{:>8}", num_words.to_string()) } else { "".to_string() },
+                        if config.bytes { format!("{:>8}", num_bytes.to_string()) } else { "".to_string() },
+                        filename_to_show);
+                }
+                if multiple {
+                    total_lines += info.as_ref().unwrap().num_lines;
+                    total_words += info.as_ref().unwrap().num_words;
+                    total_bytes += info.as_ref().unwrap().num_bytes;
+                    total_chars += info.as_ref().unwrap().num_chars;
+                }
+            },
+        }
+    }
+    if multiple {
+        if config.chars {
+            println!("{:>8} total", total_chars.to_string());
+        } else {
+            println!("{}{}{} total",
+                if config.lines { format!("{:>8}", total_lines.to_string()) } else { "".to_string() },
+                if config.words { format!("{:>8}", total_words.to_string()) } else { "".to_string() },
+                if config.bytes { format!("{:>8}", total_bytes.to_string()) } else { "".to_string() });
+        }
+    }
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _   => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+
+    let string = std::io::read_to_string(file)?;
+    for line in string.clone().lines() {
+        num_lines += 1;
+        num_words += line.split_whitespace().count();
+    }
+    num_bytes = string.clone().len();
+    num_chars = string.clone().chars().count();
+
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+        assert!(info.is_ok());
+        let expected = FileInfo {
+            num_lines: 1,
+            num_words: 10,
+            num_bytes: 48,
+            num_chars: 48,
+        };
+        assert_eq!(info.unwrap(), expected);
+    }
+}
